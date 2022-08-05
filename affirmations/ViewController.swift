faa01dde53f0be1fc217fa2e828616a0e0822990
Destiny Serna <destinyeserna@gmail.com>
2022-08-04 21:06:11 -0700

    Initial Commit

---
+++ affirmations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  affirmations
+//
+//  Created by Destiny Serna on 8/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
